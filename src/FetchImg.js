670dd3adef50b8b2f303f8f6cf6a6b017a428ea3
Pratham Kumar Singh <sprathamk@gmail.com>
2023-09-01 01:32:22 +0530

    fixed some variable names in FetchImg.js

--- src/FetchImg.js
+++ src/FetchImg.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react'
 import EditableText from './EditableText';
 function FetchImg() {
 
-    const [imge, setImg] = useState([]);
+    const [FetchImage, setFetchImage] = useState([]);
     const URL = 'https://api.unsplash.com/photos/random/?client_id=tIUT1yvfXw7gY_4PsyiYCqWUMfdYtr8FipVlzYPYDoo'
     useEffect(() => {
         const fetchData = async () => {
             const result = await fetch(URL)
             result.json().then(json => {
-                setImg(json.urls.raw);
+                setFetchImage(json.urls.raw);
             })
         }
         fetchData();
@@ -16,7 +16,7 @@ function FetchImg() {
 
     return (
         <div className='w-full h-screen'>
-            <img className="object-cover w-full h-full" src={imge} alt="An image from unspalsh API"></img>
+            <img className="object-cover w-full h-full" src={FetchImage} alt="An image from unspalsh API"></img>
             <EditableText />
         </div>
     )
